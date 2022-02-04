ce698c86f9d86228924b2d1c8fcd1d1dce4ac2d6
SebastianBoettger <60438900+SebastianBoettger@users.noreply.github.com>
2022-02-04 23:01:36 +0100

    init commit

---
+++ DemoLibrary/PersonModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DemoLibrary
+{
+    internal class PersonModel
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
